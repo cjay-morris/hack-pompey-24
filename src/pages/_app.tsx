f7344fd1ffd31f6d75308b696d56534932186842
cjay-morris <91533218+cjay-morris@users.noreply.github.com>
2024-03-16 10:42:23 +0000

    feat: implement auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,14 @@
 import { type AppType } from "next/app";
-import { Inter } from "next/font/google";
+import { SessionProvider } from "next-auth/react";
 
 import "~/styles/globals.css";
 
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
-
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <main className={`font-sans ${inter.variable}`}>
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
-    </main>
+    </SessionProvider>
   );
-};
+};  
 
 export default MyApp;
